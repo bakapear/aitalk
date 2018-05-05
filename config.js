bdcf8d7e0a94d4c04612c9490e01d1fe3aa09d8f
bakapear <proxybomb@gmail.com>
2018-05-05 15:50:55 +0200

    hardcoded some avatars

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ module.exports = {
     user: process.env.API_USER,
     key: process.env.API_KEY,
     botX: "Perl",
-    botX_avt: "",
+    botX_avt: "https://i.imgur.com/Q6gsf9U.png",
     botY: "Mika",
-    botY_avt: "",
+    botY_avt: "https://i.imgur.com/ONFFArz.png",
     startMsg: "Hello",
     hookURL: process.env.HOOK_URL
 }
