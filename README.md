0b1b4cc794e9cbf927e8ee9804eafd32e228d37c
bakapear <proxybomb@gmail.com>
2018-05-05 14:51:55 +0200

    added webhooks

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # aitalk
 2 bots from cleverbot.io talking to eachother
+
+Sends information to discord webhook x3.
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+let got = require("got")
 let cleverbot = require("cleverbot.io")
+
 let config = {
     user: process.env.API_USER,
     key: process.env.API_KEY,
     botX: "Perl",
+    botX_avt: "",
     botY: "Mika",
-    startMsg: "Hello"
+    botY_avt: "",
+    startMsg: "Hello",
+    hookURL: process.env.HOOK_URL
 }
 
 //Create first bot
@@ -18,16 +23,18 @@ botY.setNick(config.botY)
 //Init both bots and send startMsg
 botX.create((errX, sessionX) => {
     botY.create((errY, sessionY) => {
-        console.log(config.botY + ":", config.startMsg)
+        post(botY, botY_avt, config.startMsg);
         askX(config.startMsg);
+        console.log(config.botY + ":", config.startMsg)
     })
 })
 
 function askX(message) {
     try {
         botX.ask(message, (err, response) => {
-            console.log(config.botX + ":", response)
+            post(botX, botX_avt, response);
             askY(response)
+            console.log(config.botX + ":", response)
         })
     } catch (e) { console.log("X - ", e) }
 }
@@ -35,8 +42,26 @@ function askX(message) {
 function askY(message) {
     try {
         botY.ask(message, (err, response) => {
-            console.log(config.botY + ":", response)
+            post(botY, botY_avt, response);
             askX(response)
+            console.log(config.botY + ":", response)
         })
     } catch (e) { console.log("Y - ", e) }
+}
+
+function post(name, avatar, message) {
+    let url = "https://discordapp.com/api/webhooks/442304255966052362/WR3qf_fRktB3amQ1LM5dVqV-yBOXpQ3beLi_fdPVgOZSDOwiKZG8VKlcWMw625inZ11x"
+    try {
+        got.post(url, {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            form: {
+                "username": name,
+                "avatar_url": avatar,
+                "content": message
+            },
+            json: true
+        })
+    } catch (e) { console.log("POST - " + e) }
 }
